f3350137a943135d159bf24f2f12b54c61ca85cb
Bucio-Rebollar <jbucio-rebo@cps.k12.il.us>
2024-09-13 12:31:06 -0500

    first commit

---
+++ collab.py
@@ -0,0 +1,39 @@
+# this is just for testing
+# for collab purposes
+# create an input for the user to input their age
+# create an input for the user to input their name
+# create an input for the user to input their favorite soNG
+
+
+# age=(input("How old are you?"))
+# name=(input ("What is your name?"))
+# song=(input("What is your favorite song?"))
+
+#recieving input
+#test
+# name=(input(f"What is your name"))
+# #returning
+# comm = int((input("How much have you sold this month?")))
+# comm = x 
+
+# # calculation portion
+
+# print(x * 13 / 100)
+
+
+# # end evualuation
+# print( ,name,"You are entitled to" comm)
+
+#final
+
+#getting data
+name=(input("What is your name?"))
+sales=int((input("How much have you generated this month?")))
+
+#working the calculations
+comm=int(sales * .13)
+
+#final amount of money leftover. 
+print(f"{name}, this is how much you generated {sales}, leaving you with this amount, {comm}")
+
+
